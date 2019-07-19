1a4228dba2af1a5babf2e37d85afa6021a26b62b
Thibault Ceulemans <thibault.ceulemans@devoteam.lu>
2019-07-19 13:56:08 +0200

    Add SendGrid spammer Add SMTP send emails

---
+++ Utilities.SendGrid.Spammer/Program.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.SendGrid.Spammer
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", true, true)
+              .AddUserSecrets<Program>()
+              .Build();
+
+            try
+            {
+                var content = File.ReadAllText(config["Email:Body:Path"], Encoding.UTF8);
+
+                List<string> tos = new List<string>();
+                if (!string.IsNullOrWhiteSpace(config["Email:Destination:csv"]) && File.Exists(config["Email:Destination:csv"]))
+                {
+                    using (var reader = new StreamReader(config["Destination:csv"]))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            var value = reader.ReadLine();
+                            if (IsValidEmail(value))
+                                tos.Add(value);
+                        }
+                    }
+                }
+                else
+                {
+                    tos.Add(config["Destination:To"]);
+                }
+                var client = new SendGridClient(config["SendGrid:Key"]);
+
+                foreach (var to in tos)
+                {
+                    try
+                    {
+                        var msg = MailHelper.CreateSingleEmail(
+                           new EmailAddress(config["Email:From"]),
+                           new EmailAddress(to),
+                           config["Email:Subject"],
+                           content,
+                           content);
+
+                        var response = await client.SendEmailAsync(msg);
+
+                        Console.WriteLine($"E-mail sent to {to}!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+
+
+                Console.WriteLine("The job is done !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Utilities.Smtp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Smtp
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", true, true)
+              .AddUserSecrets<Program>()
+              .Build();
+
+            try
+            {
+                using (var smtpClient = new SmtpClient(config["Smtp:Server"])
+                {
+                    Credentials = !string.IsNullOrWhiteSpace(config["Smtp:Username"]) ? new NetworkCredential(config["Smtp:Username"], config["Smtp:Password"]) : null,
+                    Port = int.Parse(config["Smtp:Port"]),
+                    EnableSsl = bool.Parse(config["Smtp:Ssl"])
+                })
+                using (var msg = new MailMessage
+                {
+                    Body = File.ReadAllText(@"c:\temp\email.html", Encoding.UTF8),
+                    From = new MailAddress(config["Email:From"]),
+                    Subject = config["Email:Subject"],
+                    IsBodyHtml = true,
+                    DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure,
+                    
+                })
+                {
+                    msg.To.Add(config["Email:To"]);
+
+                    await smtpClient.SendMailAsync(msg).ConfigureAwait(false);
+                }
+
+                Console.WriteLine("E-mail sent !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
